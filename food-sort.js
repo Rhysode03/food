61ab6cb4e0ea651e9026dc3525052696b4c89699
Kyle Calder <kylebcalder03@gmail.com>
2021-09-22 01:53:28 -0600

    added files

---
+++ food-sort.js
@@ -0,0 +1,16 @@
+let select = document.getElementById("selectFood");
+let food = ["Burger King", "Carl's Jr, Wendy's", "McDonalds", "Taco Time", "Taco Bell", "Panda Express"];
+
+for (var i = 0; i < food.length; i++) {
+    let choice = food[i];
+    let el = document.createElement("option");
+    el.textContent = choice;
+    el.value = choice;
+    select.appendChild(el);
+}
+let x = document.getElementById("food").innerHTML = choice;
+
+function getRandomFood() {
+    let random = Math.floor(Math.random() * food.length);
+    document.getElementById("food").innerHTML = food[random];
+}
